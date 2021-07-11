26500794b63f12d83ac861393d5aead5a2f948d2
swy <Songwy27@163.com>
2021-07-11 18:04:32 +0800

    second commit

--- src/main/java/com/xx/git/GitTest.java
+++ src/main/java/com/xx/git/GitTest.java
@@ -3,5 +3,6 @@ package com.xx.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("hello word2");
     }
 }
